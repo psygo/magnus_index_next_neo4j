50fe1a486c6f1596a5350e33b98000974391548f
Philippe Fanaro <philippefanaro@gmail.com>
2023-10-14 18:56:14 -0300

    first iteration of a graph

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import ForceGraph3D from "react-force-graph-3d";
+
 export default function Page() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function getGraphData() {
+      const res = await fetch(
+        "http://localhost:3000/api/graph"
+      );
+      const data = await res.json();
+
+      setData(data);
+    }
+
+    getGraphData();
+  }, []);
+
   return (
     <main>
-      <h1>Magnus Index</h1>
+      <ForceGraph3D graphData={data} />,
     </main>
   );
 }
