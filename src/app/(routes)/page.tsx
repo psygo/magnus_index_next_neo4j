d1088ec7b41d2ded7029aafcb53a43a9cbb39548
Philippe Fanaro <philippefanaro@gmail.com>
2023-10-17 17:13:29 -0300

    trying to fix a react-force-graph dynamic import issue

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,10 +2,20 @@
 
 import { useGraphData } from "./utils/use_graph_data";
 
-import { Graph2d } from "./components/Graph2d";
+import dynamic from "next/dynamic";
+
+const NoSSRForceGraph2D = dynamic(
+  () =>
+    import("./components/Graph2d").then(
+      (module) => module.Graph2d
+    ),
+  { ssr: false }
+);
 
 export default function Page() {
   const data = useGraphData();
 
-  return <main>{data && <Graph2d data={data} />}</main>;
+  return (
+    <main>{data && <NoSSRForceGraph2D data={data} />}</main>
+  );
 }
