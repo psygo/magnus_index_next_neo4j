6c2a0f8a3938511f6c52b89433b87a49c4b048f9
Philippe Fanaro <philippefanaro@gmail.com>
2023-10-17 15:15:27 -0300

    how to connect to a Neo4j AURA deployment

--- src/app/lib/config/db.ts
+++ src/app/lib/config/db.ts
@@ -3,12 +3,22 @@ import * as neo4j from "neo4j-driver";
 function neo4jSetup() {
   const neo4jPort = parseInt(process.env.NEO4J_PORT!);
   const uri = `neo4j://localhost:${neo4jPort}`;
+
+  const remoteUriPrefix = "neo4j+s://";
+  const remoteUri = `${remoteUriPrefix}2a8bc089.databases.neo4j.io:7687`;
+
   const user = process.env.NEO4J_USER!;
   const password = process.env.NEO4J_PASSWORD!;
 
+  const remoteUser = process.env.AURA_USERNAME!;
+  const remotePassword = process.env.AURA_PASSWORD!;
+  
+  console.log(remoteUser)
+  console.log(remotePassword)
+
   const driver = neo4j.driver(
-    uri,
-    neo4j.auth.basic(user, password)
+    remoteUri,
+    neo4j.auth.basic(remoteUser, remotePassword)
   );
   const session = driver.session();
 
