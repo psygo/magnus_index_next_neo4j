33c406f43ff8a21de98f55680092b969d6e64f72
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-09 10:10:59 -0300

    managed to more or less do a hierarchy of types for later on

--- src/app/lib/models/graph2.ts
+++ src/app/lib/models/graph2.ts
@@ -1,4 +1,4 @@
-import { NeoNodeLabel } from "./graph";
+import { Id, NeoLinkLabel, NeoNodeLabel } from "./graph";
 
 export interface OutNode<N extends NeoNodeLabel> {
   type: N;
@@ -16,3 +16,29 @@ export interface UserNode {
   type: NeoNodeLabel.User;
   properties: UserProperties;
 }
+
+//----------------------------------------------------------
+
+export interface OutLink<
+  L extends NeoLinkLabel,
+  S extends NeoNodeLabel,
+  T extends NeoNodeLabel
+> {
+  type: L;
+  source: Id | OutNode<S>;
+  target: Id | OutNode<T>;
+  properties: LinkProperties<L>;
+}
+
+export interface LinkProperties<L extends NeoLinkLabel> {}
+
+export interface FollowsProperties
+  extends LinkProperties<NeoLinkLabel.Follows> {
+  created_at: number;
+}
+export interface FollowsLink
+  extends OutLink<
+    NeoLinkLabel.Follows,
+    NeoNodeLabel.User,
+    NeoNodeLabel.User
+  > {}
