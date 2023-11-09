d497f4d12d96978550331fac7c8953421316a3fd
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-09 15:15:41 -0300

    renaming middleware

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -4,7 +4,7 @@ import { neo4jSession } from "@/lib/config/db";
 
 import { getAllNodes } from "@/lib/utils/neo4j_utils";
 
-import { createContentMentions } from "@/lib/api_helpers/content_mentions/exports";
+import { createContentMentions } from "@/lib/middleware/content_mentions/exports";
 
 /**
  * Create Item
